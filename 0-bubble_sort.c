dee6c7d3fa6cbb365aab4a05ce7effcf61367ad2
somaftees <somaftees@gmail.com>
2023-11-20 12:14:49 -0800

    done

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	i = 0;
 	while (i < size)
 	{
-		for (j = 0; j< size - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
